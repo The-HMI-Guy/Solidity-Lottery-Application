eee25a4d9073e3e735648dc0f8828b048df82c40
Ray Ortiz <oray@bgsu.edu>
2022-02-19 09:56:02 -0500

    adding more comments

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.17;
 
 contract Lottery {
+    //Manager is the address of the person who created the contract
+    //Players array will contain the addresses of the players who have entered the lottery
+        //Address array is dynamic 
     address public manager;
     address[] public players;
     
@@ -8,11 +11,14 @@ contract Lottery {
         manager = msg.sender;
     }
     
+    //Players must enter a minimum of .01 ETH
+    //If address meets this requirement, the address is added to the players array
     function enter() public payable {
         require(msg.value > .01 ether);
         players.push(msg.sender);
     }
-    
+    //Solidity does not have a random generator
+    //Pseudo randon number generator was used, which COULD be calculated by a player
     function random() private view returns (uint) {
         return uint(keccak256(block.difficulty, now, players));
     }
