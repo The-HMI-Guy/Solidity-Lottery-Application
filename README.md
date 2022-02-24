934054ec53b3ceba37622c673ceb702b2b8707c9
Ray Ortiz <oray@bgsu.edu>
2022-02-24 07:35:58 -0500

    update readme.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Solidity-Lottery-Application
-Application was built from the Udemy course by Stephen Grider
+# A Lottery Application Built Using Solidity
+***
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+## General info
+This project was built from the Udemy course by Stephen Grider. Over time, this project will branch off from the course and reflect personal features. 
+	
+## Technologies
+Project is created with:
+* Solidity Version: 0.4.17
+* NPM Version: 8.1.2
+* Ganache CLI Version: 6.12.2
+* Mocha Version: 9.1.2
+* Web3 Version: 1.6.0
+* Truffle Provider Version: 1.5.1
+	
+## Setup
+Coming soon!
--- deploy.js
+++ deploy.js
@@ -1,30 +1,29 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
 
 //Connect to a target network (Rinkeby) and unlock an account to use on that network.
 //Account was unlocked by providing the mnemonic phrase below.
-//Unlocking the account gives access to the public key, private key, and address. 
+//Unlocking the account gives access to the public key, private key, and address.
 const provider = new HDWalletProvider(
-    //Metamask account for this project is for development purposes only.
-    //Replace mnemonic phrase below with your personal phrase.
-    'air praise perfect woman noise zoo nominee explain radar add globe act',
-    'https://rinkeby.infura.io/v3/2cf2bbd4068f45c08aeb064ce51468ed' //Specify the node to use.
-
+  //Metamask account for this project is for development purposes only.
+  //Replace mnemonic phrase below with your personal phrase.
+  "air praise perfect woman noise zoo nominee explain radar add globe act",
+  "https://rinkeby.infura.io/v3/2cf2bbd4068f45c08aeb064ce51468ed" //Specify the node to use.
 );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+    .send({ gas: "1000000", from: accounts[0] });
 
-  console.log(interface);
-  console.log('Contract deployed to', result.options.address);
+  console.log(interface); //Interface copy/pasted to react file.
+  console.log("Contract deployed to", result.options.address); //Address the contract was deployed to copy/pasted to react file.
   provider.engine.stop();
 };
 deploy();
