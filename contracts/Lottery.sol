3aa862b6425610f44d4c08b15ca07693cc7164d9
Ray Ortiz <oray@bgsu.edu>
2022-03-02 21:33:48 -0500

    store winner's address to be displayed in react

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,43 +5,48 @@ contract Lottery {
     //Players array will contain the addresses of the players who have entered the lottery.
     address public manager;
     address[] public players;
-    
+
     //First address creating the lottery will become the manager.
-    //The manager will have admin abilities, which outlined below. 
+    //The manager will have admin abilities, which outlined below.
     function Lottery() public {
         manager = msg.sender;
     }
-    
+
     //Players must enter a minimum of .01 ETH.
     //If address meets this requirement, the address is added to the players array.
     function enter() public payable {
         require(msg.value > .01 ether);
         players.push(msg.sender);
     }
+
     //Solidity does not have a random generator.
     //Pseudo random number generator was used, which COULD be calculated by a player.
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+    function random() private view returns (uint256) {
+        return uint256(keccak256(block.difficulty, now, players));
     }
+
     //This function will only run if the "manager" calls it.
     //Store the index of the winner using the function random.
-    //Use the player's hash to access the transfer function, which will transfer the lottery's balance 
+    //Use the player's hash to access the transfer function, which will transfer the lottery's balance
     //to the winner's address.
     //Reset players array and create new dynamic array.
     function pickWinner() public restricted {
-        uint index = random() % players.length;
+        uint256 index = random() % players.length;
         players[index].transfer(this.balance);
+        lastWinner = players[index];
         players = new address[](0);
     }
+
     //Check to see if the address is the manager.
     //Modifier function reduces the amount of code needed in a contract.
     modifier restricted() {
         require(msg.sender == manager);
-        _; //Solidity places the code block here that is calling the function. 
+        _; //Solidity places the code block here that is calling the function.
     }
+
     //Returns the list off players who have entered the active lottery.
     //View keyword indicates the function will not change any data.
     function getPlayers() public view returns (address[]) {
         return players;
     }
-}   
+}
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,11 +1,11 @@
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3'); //Portal to the Etherum network (retrieve and make changes).
-const web3 = new Web3(ganache.provider()); //Ganache is the host network. Send and deploy contracts to the Ganache network. 
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3"); //Portal to the Etherum network (retrieve and make changes).
+const web3 = new Web3(ganache.provider()); //Ganache is the host network. Send and deploy contracts to the Ganache network.
 
 //Interface = ABI. Translation layer that communication data from the network to JS.
 //Bytecode = Compiled contract.
-const { interface, bytecode } = require('../compile'); 
+const { interface, bytecode } = require("../compile");
 
 let lottery;
 let accounts;
@@ -13,95 +13,92 @@ let accounts;
 //This block of code is executed before each test.
 //Each test will start with a new contract.
 beforeEach(async () => {
-    
-    accounts = await web3.eth.getAccounts();//Get a list of all accounts
+  accounts = await web3.eth.getAccounts(); //Get a list of all accounts
 
-    //Use one of those accounts to deploy the contract.
-    lottery = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode })
-        .send({ from: accounts[0], gas: '1000000'});
+  //Use one of those accounts to deploy the contract.
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 //.methods allows us to access the functions created in the contract.
 //.call() = read-only. Instantaneous.
 //.send() = change to the data in the contract. Not instantaneous.
-describe('Lottery Contract', () => {
-    it('deploys a contract', () => {
-        assert.ok(lottery.options.address);
-    });
-    it('allows one account to enter', async () => {
-        await lottery.methods.enter().send({
-            from: accounts[0],
-            value: web3.utils.toWei('0.02', 'ether')
-        });
-
-        const players = await lottery.methods.getPlayers().call({
-            from: accounts[0]
-        });
-
-        assert.equal(accounts[0], players[0]);
-        assert.equal(1, players.length)
-    });
-    it('allows multiple account to enter', async () => {
-        await lottery.methods.enter().send({
-            from: accounts[0],
-            value: web3.utils.toWei('0.02', 'ether')
-        });
-        await lottery.methods.enter().send({
-            from: accounts[1],
-            value: web3.utils.toWei('0.02', 'ether')
-        });
-        await lottery.methods.enter().send({
-            from: accounts[2],
-            value: web3.utils.toWei('0.02', 'ether')
-        });
-
-        const players = await lottery.methods.getPlayers().call({
-            from: accounts[0]
-        });
-
-        assert.equal(accounts[0], players[0]);
-        assert.equal(accounts[1], players[1]);
-        assert.equal(accounts[2], players[2]);
-        assert.equal(3, players.length)
+describe("Lottery Contract", () => {
+  it("deploys a contract", () => {
+    assert.ok(lottery.options.address);
+  });
+  it("allows one account to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether"),
     });
 
-    it('requires a minimum amount of ether', async () => {
-        try {
-            await lottery.methods.enter().send({
-                from: accounts[0],
-                value: 0
-        });
-        assert(false);
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
+  });
+  it("allows multiple account to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether"),
+    });
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei("0.02", "ether"),
+    });
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei("0.02", "ether"),
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3, players.length);
+  });
+
+  it("requires a minimum amount of ether", async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 0,
+      });
+      assert(false);
     } catch (err) {
-        assert.ok(err);
-        }
+      assert.ok(err);
+    }
+  });
+
+  it("only manager can call pickWinner", async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("sends money to the winner and resets the players array", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("2", "ether"),
     });
 
-    it('only manager can call pickWinner', async () => {
-        try{
-            await lottery.methods.pickWinner().send({
-                from: accounts[1]
-            });
-            assert(false);
-        } catch (err) {
-            assert(err);
-        }
-    });
-
-    it('sends money to the winner and resets the players array', async () => {
-        await lottery.methods.enter().send({
-            from: accounts[0],
-            value: web3.utils.toWei('2', 'ether')
-        });
-
-        const initialBalance = await web3.eth.getBalance(accounts[0]);
-
-        await lottery.methods.pickWinner().send({ from: accounts[0]});
-
-        const finalBalance = await web3.eth.getBalance(accounts[0]);
-
-        const difference = finalBalance - initialBalance;
-        assert(difference > web3.utils.toWei('1.8', 'ether'));
-    });
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+    
+    assert(difference > web3.utils.toWei("1.8", "ether"));
+  });
 });
