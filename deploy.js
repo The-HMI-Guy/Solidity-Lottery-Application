942df370b1d0cdaab8e1d6bf07c3925fd81f8621
Ray Ortiz <oray@bgsu.edu>
2022-03-04 15:57:32 -0500

    Searching for bug

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
 
 //Connect to a target network (Rinkeby) and unlock an account to use on that network.
 //Account was unlocked by providing the mnemonic phrase below.
@@ -8,22 +8,22 @@ const { interface, bytecode } = require("./compile");
 const provider = new HDWalletProvider(
   //Metamask account for this project is for development purposes only.
   //Replace mnemonic phrase below with your personal phrase.
-  "air praise perfect woman noise zoo nominee explain radar add globe act",
-  "https://rinkeby.infura.io/v3/2cf2bbd4068f45c08aeb064ce51468ed" //Specify the node to use.
+  'air praise perfect woman noise zoo nominee explain radar add globe act',
+  'https://rinkeby.infura.io/v3/2cf2bbd4068f45c08aeb064ce51468ed' //Specify the node to use.
 );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account", accounts[0]);
+  console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ gas: "1000000", from: accounts[0] });
+    .send({ gas: '1000000', from: accounts[0] });
 
   console.log(interface); //Interface copy/pasted to react file.
-  console.log("Contract deployed to", result.options.address); //Address the contract was deployed to copy/pasted to react file.
+  console.log('Contract deployed to', result.options.address); //Address the contract was deployed to copy/pasted to react file.
   provider.engine.stop();
 };
 deploy();
